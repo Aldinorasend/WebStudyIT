1943cace8e17b6394e40e26d9df8ffffb255268e
PajarAland <162042636+PajarAland@users.noreply.github.com>
2024-12-28 02:54:36 +0700

    tes

---
+++ resources/views/modul/index.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Course View</title>
+    <link rel="stylesheet" href="{{ asset('css/stylesCor.css') }}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="banner">
+        <img src="{{ asset('image/glenn-carstens-peters-P1qyEf1g0HU-unsplash') }}" alt="Banner Image">
+        <div class="banner-text">UI-UX Beginner Class</div>
+    </div>
+
+    <div class="arw-back">
+        <a href="../page/homePage.html">
+            <img src="../asset/image/backarrow.png" alt="Back to Course View">
+        </a>
+    </div>
+
+    
+        <div class="card">
+            <img src="{{ asset('image/glenn-carstens-peters-P1qyEf1g0HU-unsplash') }}" class="card-img-top" alt="Course Image">
+            <div class="card-body">
+                <h5 class="card-title" style="color: white;">UI-UX Beginner Class</h5>
+                <p class="calendar-text">
+                    <img src="{{ asset('image/calen.png') }}" alt="Calendar Icon" class="calendar-icon">
+                    25-26 August 2024
+                </p>
+            </div>
+        </div>
+    
+
+    <div class="dec-container">
+        <p>What You'll Learn</p>
+    </div>
+
+    <div class="des-container">
+        <p>1. The principles of user-centered design</p>
+        <p>2. How to conduct user research and usability testing</p>
+        <p>3. Creating effective user personas and journey maps</p>
+        <p>4. Designing intuitive user interfaces</p>
+        <p>5. Prototyping and wireframing with industry-standard tools</p>
+    </div>
+
+ 
+    <div class="des-tools">
+        <p>Here are some tools that you'll learn in this course</p>
+        <div class="img-tools">
+            <img src="../asset/image/figmaaa.png" alt="Figma Icon" class="img-icon">
+            <img src="../asset/image/adobe1.jpg" alt="Adobe Icon" class="img-icon">
+        </div>
+    </div>
+
+  
+    <div class="enroll-container">
+        <a href="../page/videoModule.html" class="btn btn-primary btn-lg">Enroll</a>
+    </div>
+</body>
+</html>
