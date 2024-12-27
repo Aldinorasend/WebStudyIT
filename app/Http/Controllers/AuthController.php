67c24237523cb1576607f02bf2c69d692b1c0cd0
SharkMouth <40568844+SharkMouth@users.noreply.github.com>
2024-12-27 16:02:44 +0700

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,15 +3,54 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Account;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
     public function login(){
-        return view('auth.login');
+        return view('Auth.login');
     }
 
     public function register(){
-        return view('auth.register');
+        return view('Auth.register');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $login = $request->input('login');
+        $password = $request->input('password');
+        
+        $response = Http::get(env('API_URL') . '/login', [
+            'login' => $login,
+            'password' => $password
+        ]);
+
+        if ($response->successful()) {
+            $user = $response->json();
+            Session::put('user', $user);
+            
+            if ($user['user_type'] === 'admin') {
+                return redirect()->route('admin.index', ['id' => $user['id']]);
+            } elseif (in_array($user['user_type'], ['free', 'subscriber'])) {
+                return redirect()->route('students.index', ['id' => $user['id']]);
+            }
+        }
+
+        return back()->withErrors([
+            'login' => 'Invalid credentials or server error.',
+        ]);
+    }
+
+    public function adminIndex($id)
+    {
+        $user = Session::get('user');
+        return view('Admin.index', ['account' => $user]);
+    }
+
+    public function studentIndex($id)
+    {
+        $user = Session::get('user');
+        return view('students.index', ['account' => $user]);
     }
 }
